eb499e47df8b8cd980c164137fd48a41b316af8a
anton.kostiuchenko <a.kostiuchenko@mytaxi.com>
2020-12-05 22:08:08 +0100

    Initial project setup

---
+++ src/main/kotlin/com/test/usersubscriptionsservice/UsersubscriptionsserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.test.usersubscriptionsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsersubscriptionsserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsersubscriptionsserviceApplication>(*args)
+}
