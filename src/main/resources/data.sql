b152f897482c1bc86bf33506bce3a62af495aa0a
anton.kostiuchenko <a.kostiuchenko@mytaxi.com>
2020-12-06 23:25:07 +0100

    Add H2 props and test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Create test data
+
+INSERT INTO PRODUCTS (id, name, description) VALUES
+  (random_uuid(), 'Full-Body Circuit', 'Get toned'),
+  (random_uuid(), 'Yoga', 'Increase flexibility'),
+  (random_uuid(), 'Fat burner', 'Lose fat');
