de786aa1acb47296efe0cf60fa86b46d84ba607b
anton.kostiuchenko <a.kostiuchenko@mytaxi.com>
2020-12-16 00:00:02 +0200

    Remove file with test data

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
--- Create test data
-
-INSERT INTO PRODUCTS (id, name, description) VALUES
-  (random_uuid(), 'Full-Body Circuit', 'Get toned'),
-  (random_uuid(), 'Yoga', 'Increase flexibility'),
-  (random_uuid(), 'Fat burner', 'Lose fat');
